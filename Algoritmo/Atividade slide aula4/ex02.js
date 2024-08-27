9410bd0440cd093ac8c327964aa7509793bb284a
Adson Filho <117692306+adsonfilho@users.noreply.github.com>
2024-08-27 12:39:24 -0300

    Add files via upload

---
+++ Algoritmo/Atividade slide aula4/ex02.js
@@ -0,0 +1,3 @@
+let nome = prompt("Entre com o nome: ");
+
+console.log("Primeiro: " + nome[0]);
---
+++ Algoritmo/Atividade slide aula4/ex03.js
@@ -0,0 +1,3 @@
+let nome = prompt("Entre com o nome: ");
+
+console.log("Ultimo: " + nome[nome.length-1]);
